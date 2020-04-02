8b562bd937f8c2920e23fbc41fcb46e6373d720e
Artur Orlov <wirewit@rohrpost>
2020-04-02 18:09:00 +0300

    Production/Stable version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="restretto",
-    version="0.4.5",
+    version="1.0.0",
     description="restretto is REST API testing tool",
-    #long_description=description,
+    long_description="YML-scenario based REST API testing tool",
     author="Arthur Orlov",
     author_email="feature.creature@gmail.com",
     url="http://github.com/wirewit/restretto",
@@ -15,7 +15,7 @@ setup(
     entry_points={"console_scripts": ["restretto = restretto.cli:main"]},
     install_requires=["requests>=2.7.0", "pyaml>=3.11", "jinja2>=2.8", "clint>=0.5"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
